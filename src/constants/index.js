12a216f81f9232e6a21a32a7cae4c9ff46d39062
Vaibhav Sanket <101280631+sanky-7@users.noreply.github.com>
2023-12-12 19:13:29 +0530

    3d react website

---
+++ src/constants/index.js
@@ -0,0 +1,238 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "Flutter Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "University Student",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "React.js Twitter Clone",
+      company_name: "",
+      icon: starbucks,
+      iconBg: "#03dac6",
+      date: "November 2021 - March 2022",
+      points: [
+        "sdccc ccccccc ccc cc sdv sdv sd vd zsd cvsdzv dsvs dvds vdss cACs vd db dfb dfbg.",
+        "sdccc ccccccc ccc cc sdv sdv sd vd zsd cvsdzv dsvs dvds vdss cACs vd db dfb dfbg.",
+        "sdccc ccccccc ccc cc sdv sdv sd vd zsd cvsdzv dsvs dvds vdss cACs vd db dfb dfbg.",
+        "sdccc ccccccc ccc cc sdv sdv sd vd zsd cvsdzv dsvs dvds vdss cACs vd db dfb dfbg.",
+      ],
+    },
+    {
+      title: "MERN Stack Website",
+      company_name: "",
+      icon: tesla,
+      iconBg: "#370083",
+      date: "May 2022 - July 2022",
+      points: [
+        "sdccc ccccccc ccc cc sdv sdv sd vd zsd cvsdzv dsvs dvds vdss cACs vd db dfb dfbg.",
+        "sdccc ccccccc ccc cc sdv sdv sd vd zsd cvsdzv dsvs dvds vdss cACs vd db dfb dfbg.",
+        "sdccc ccccccc ccc cc sdv sdv sd vd zsd cvsdzv dsvs dvds vdss cACs vd db dfb dfbg.",
+        "sdccc ccccccc ccc cc sdv sdv sd vd zsd cvsdzv dsvs dvds vdss cACs vd db dfb dfbg.",
+      ],
+    },
+    {
+      title: "Amazon Clone using React.js",
+      company_name: "",
+      icon: shopify,
+      iconBg: "#cf6679",
+      date: "September 2022 - December 2022",
+      points: [
+        "sdccc ccccccc ccc cc sdv sdv sd vd zsd cvsdzv dsvs dvds vdss cACs vd db dfb dfbg.",
+        "sdccc ccccccc ccc cc sdv sdv sd vd zsd cvsdzv dsvs dvds vdss cACs vd db dfb dfbg.",
+        "sdccc ccccccc ccc cc sdv sdv sd vd zsd cvsdzv dsvs dvds vdss cACs vd db dfb dfbg.",
+        "sdccc ccccccc ccc cc sdv sdv sd vd zsd cvsdzv dsvs dvds vdss cACs vd db dfb dfbg.",
+      ],
+    },
+    {
+      title: "Flutter AI Chat App",
+      company_name: "",
+      icon: meta,
+      iconBg: "#bb86fc",
+      date: "January 2023 - February 2023",
+      points: [
+        "sdccc ccccccc ccc cc sdv sdv sd vd zsd cvsdzv dsvs dvds vdss cACs vd db dfb dfbg.",
+        "sdccc ccccccc ccc cc sdv sdv sd vd zsd cvsdzv dsvs dvds vdss cACs vd db dfb dfbg.",
+        "sdccc ccccccc ccc cc sdv sdv sd vd zsd cvsdzv dsvs dvds vdss cACs vd db dfb dfbg.",
+        "sdccc ccccccc ccc cc sdv sdv sd vd zsd cvsdzv dsvs dvds vdss cACs vd db dfb dfbg.",
+      ],
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Car Rent",
+      description:
+        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Job IT",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, projects };
